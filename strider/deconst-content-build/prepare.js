dbf7000b07f9dd073e1f79f5b8687c57c81c98c3
Ash Wilson <smashwilson@gmail.com>
2016-01-12 09:30:09 -0500

    Pull first and log the container's output.

--- strider/deconst-content-build/prepare.js
+++ strider/deconst-content-build/prepare.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
-var dockerode = require('dockerode');
+var stream = require('stream');
+var Docker = require('dockerode');
 var async = require('async');
 
 var logger = require('./logger');
@@ -22,6 +23,12 @@ var preparerWhitelist = (function () {
 })();
 
 exports.connect = function () {
+  logger.info("Connecting to Docker", {
+    DOCKER_HOST: process.env.DOCKER_HOST,
+    DOCKER_TLS_VERIFY: process.env.DOCKER_TLS_VERIFY,
+    DOCKER_CERT_PATH: process.env.DOCKER_CERT_PATH
+  });
+
   docker = new Docker();
 };
 
@@ -45,42 +52,109 @@ var choosePreparer = function (state) {
         }
 
         state.preparer = config.preparer;
-        return callback(null);
+        logger.info('Using configured preparer: %s', state.preparer);
+        callback(null);
       } else {
         // Infer from directory contents
         heuristic.guessPreparer(state.root, function (err, preparer) {
+          if (err) return callback(err);
+
           state.preparer = preparer;
-          return callback(err);
+          logger.info('Using inferred preparer: %s', state.preparer);
+          callback(null);
         });
       }
     });
   };
 };
 
+var pullPreparerContainer = function (state) {
+  return function (callback) {
+    logger.debug("Pulling latest preparer container image.");
+
+    docker.pull(state.preparer, function (err, stream) {
+      if (err) return callback(err);
+
+      var onProgress = function (e) {
+        // This is noisy, so do nothing.
+      };
+
+      var onFinished = function (err, output) {
+        if (err) return callback(err);
+
+        logger.debug("Preparer container image pulled.");
+        callback(null);
+      };
+
+      docker.modem.followProgress(stream, onFinished, onProgress);
+    });
+  };
+};
+
 var createPreparerContainer = function (state) {
   return function (callback) {
-    docker.createContainer({
+    var params = {
       Image: state.preparer,
       Env: [
-        "CONTENT_STORE_URL=",
-        "CONTENT_STORE_APIKEY=",
+        "CONTENT_STORE_URL=" + (process.env.CONTENT_STORE_URL || ''),
+        "CONTENT_STORE_APIKEY=" + (process.env.CONTENT_STORE_APIKEY || ''),
         "TRAVIS_PULL_REQUEST=false"
       ]
-    }, function (err, container) {
+    };
+
+    logger.debug("Creating preparer container.", params);
+
+    docker.createContainer(params, function (err, container) {
+      if (err) return callback(err);
+
       state.container = container;
-      callback(err);
+      callback(null);
+    });
+  };
+};
+
+var preparerContainerLogs = function (state) {
+  return function (callback) {
+    logger.debug("Reporting logs from preparer container.", {
+      containerId: state.container.id
+    });
+
+    var logStream = new stream.PassThrough();
+
+    logStream.on('data', function (chunk) {
+      process.stdout.write(chunk.toString('utf-8'));
+    });
+
+    state.container.logs({
+      follow: true,
+      stdout: true,
+      stderr: true
+    }, function (err, stream) {
+      if (err) return callback(err);
+
+      state.container.modem.demuxStream(stream, logStream, logStream);
+
+      callback(null);
     });
   };
 };
 
 var startPreparerContainer = function (state) {
   return function (callback) {
+    logger.debug("Starting preparer container.", {
+      containerId: state.container.id
+    });
+
     state.container.start(callback);
   };
 };
 
 var waitForCompletion = function (state) {
   return function (callback) {
+    logger.debug("Waiting for preparer container completion.", {
+      containerId: state.container.id
+    });
+
     state.container.wait(function (err, result) {
       state.status = result.StatusCode;
       callback(null);
@@ -92,13 +166,26 @@ exports.prepare = function (root, callback) {
   var state = {
     root: root,
     container: null,
-    status: 0
+    status: null
   };
 
   async.series([
     choosePreparer(state),
+    pullPreparerContainer(state),
     createPreparerContainer(state),
     startPreparerContainer(state),
+    preparerContainerLogs(state),
     waitForCompletion(state)
-  ], callback);
+  ], function (err) {
+    if (err) {
+      logger.error("Preparer completed with an error.", err);
+      return callback(err);
+    }
+
+    logger.info("Preparer completed.", {
+      status: state.status
+    });
+
+    callback(null);
+  });
 };
