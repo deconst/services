ff3f9d5320c21b2aed4665d155f3ae6f11a0ea0a
Ash Wilson <smashwilson@gmail.com>
2016-01-13 15:13:03 -0500

    Invoke strider more simply

--- strider/deconst-strider/index.js
+++ strider/deconst-strider/index.js
@@ -5,11 +5,4 @@ var strider = require('strider');
 
 var extPath = path.join(__dirname, "node_modules");
 
-strider(extPath, {}, function (err, initialized, appInstance) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  console.log("Strider is up and running.");
-});
+strider(extPath);
