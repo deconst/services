d6dac7d163822396f3f6d5ffce675157461084f0
Ash Wilson <smashwilson@gmail.com>
2016-01-25 12:23:00 -0500

    Maybe work with Enterprise

--- strider/deconst-strider/config.js
+++ strider/deconst-strider/config.js
@@ -1,4 +1,13 @@
+function withoutTrailingSlash (original) {
+  if (original[original.length - 1] === '/') {
+    return original.substr(0, original.length - 1);
+  } else {
+    return original;
+  }
+}
+
 exports.striderSystemEmail = process.env.STRIDER_SYSTEM_EMAIL;
 
 exports.githubUsername = process.env.GITHUB_SYSTEM_USERNAME;
 exports.githubToken = process.env.GITHUB_SYSTEM_TOKEN;
+exports.githubAPIEndpoint = withoutTrailingSlash(process.env.GITHUB_API_ENDPOINT || 'https://api.github.com');
