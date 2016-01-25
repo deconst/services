24f065e9e2c036d42ee966e772b49274886cd91c
Ash Wilson <smashwilson@gmail.com>
2016-01-25 13:30:23 -0500

    We don't need a username, just the token.

--- strider/deconst-strider/config.js
+++ strider/deconst-strider/config.js
@@ -8,6 +8,5 @@ function withoutTrailingSlash (original) {
 
 exports.striderSystemEmail = process.env.STRIDER_SYSTEM_EMAIL;
 
-exports.githubUsername = process.env.GITHUB_SYSTEM_USERNAME;
 exports.githubToken = process.env.GITHUB_SYSTEM_TOKEN;
 exports.githubAPIEndpoint = withoutTrailingSlash(process.env.GITHUB_API_ENDPOINT || 'https://api.github.com');
